37f655b2d2748710907868a1f8b8f3bb20d09532
tastyTour <94881325+tastyTour@users.noreply.github.com>
2021-11-24 14:54:29 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,5 +45,5 @@ async def on_message(message):
         await message.channel.send(embed=embed)
 
 
-access_token = os.environ["BOT_TOKEN"]     
+access_token = os.environ["token"]     
 client.run(access_token)
